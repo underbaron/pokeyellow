450a4d503d5438c2f8a19c362446b3f05d9874c0
underbaron <123493692+underbaron@users.noreply.github.com>
2023-06-23 20:19:50 +0200

    Update menu_icons.asm

--- data/pokemon/menu_icons.asm
+++ data/pokemon/menu_icons.asm
@@ -151,4 +151,6 @@ MonPartyData:
 	nybble ICON_SNAKE     ; Dragonite
 	nybble ICON_MON       ; Mewtwo
 	nybble ICON_MON       ; Mew
+	nybble ICON_QUADRUPED ; Electrike
+	nybble ICON_QUADRUPED ; Manectric
 	end_nybble_array NUM_POKEMON
